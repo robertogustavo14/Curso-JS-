691782e45982a65d2606f7b9a3288f30371c5f25
robertogustavo14 <robertogustavo14@gmail.com>
2021-03-26 00:27:03 -0300

    Adicionado aula 13 e 14

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,10 @@
+var inicio = document.getElementById("inicio")
+var fim = document.getElementById("fim")
+var passo = document.getElementById("passo")
+var resu = document.getElementById("cont")
+function contar (){
+for (inicio; inicio <= fim ; inicio + passo ) {
+    resu.innerHTML = ("testando",inicio)
+
+}
+}
